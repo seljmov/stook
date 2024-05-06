cd56fcc55a69f219f446c7eba98362980c689f57
Zagidin Selimov <seljmov@list.ru>
2024-05-06 19:00:43 +0300

    create stook_shared library

---
+++ stook-mobile-app/packages/stook_shared/lib/src/task_status.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+/// Возвращает статусы для планирования.
+List<TaskStatus> get planningStatuses =>
+    [TaskStatus.pending, TaskStatus.inProgress];
+
+/// Статус задачи.
+enum TaskStatus {
+  /// Задача создана, но еще не начата.
+  pending,
+
+  /// Задача в процессе выполнения.
+  inProgress,
+
+  /// Задача завершена.
+  completed,
+
+  /// Задача просрочена.
+  overdue,
+}
+
+/// Расширение для работы с перечислением [TaskStatus].
+extension TaskStatusExtension on TaskStatus {
+  /// Возвращает строковое представление перечисления.
+  String get name {
+    switch (this) {
+      case TaskStatus.pending:
+        return 'Создана';
+      case TaskStatus.inProgress:
+        return 'В процессе';
+      case TaskStatus.completed:
+        return 'Завершена';
+      case TaskStatus.overdue:
+        return 'Просрочена';
+      default:
+        throw Exception('Unknown task status: $this');
+    }
+  }
+
+  /// Возвращает цвет для перечисления.
+  Color get color {
+    switch (this) {
+      case TaskStatus.pending:
+        return Colors.blue;
+      case TaskStatus.inProgress:
+        return Colors.orange;
+      case TaskStatus.completed:
+        return Colors.green;
+      case TaskStatus.overdue:
+        return Colors.red;
+      default:
+        throw Exception('Unknown task status: $this');
+    }
+  }
+
+  /// Возвращает индекс сортировки для перечисления.
+  int get sortIndex => switch (this) {
+        TaskStatus.inProgress => 0,
+        TaskStatus.pending => 1,
+        TaskStatus.overdue => 2,
+        TaskStatus.completed => 3,
+      };
+}
---
+++ stook-mobile-app/packages/stook_shared/lib/stook_shared.dart
@@ -0,0 +1,4 @@
+library stook_shared;
+
+export 'src/task_priority.dart';
+export 'src/task_status.dart';
